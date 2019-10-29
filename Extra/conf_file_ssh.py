8d5c18229eb1454fa1da22e194c9cb7436fcae3e
FraPsiZeta <fra.quarta7@gmail.com>
2019-10-29 21:28:52 +0100

    remove Extra

--- Extra/conf_file_ssh.py
+++
@@ -1,5 +0,0 @@
-device_info_ssh = {
-                    "device_ssh_passwd" : "ninonino",
-                    "device_ssh_user" : "francesco",
-                    "device_ssh_ip" : "137.204.240.77"
-                    }
